b883ca2917b768b353237f601dd6aa8cb06b9f64
Dev Ashar <dev.ashar2019@vitstudent.ac.in>
2022-08-16 11:27:14 +0530

    feat: todo form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Button, Card, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoForm from "./components/TodoForm/TodoForm";
 function App() {
   const [todos, setTodos] = useState([]);
   const [todoInput, setTodoInput] = useState("");
@@ -9,6 +10,7 @@ function App() {
     <div className="app">
       <div className="container">
         <h1 className="text-center mb-4">Todo List</h1>
+        <TodoForm/>
       </div>
     </div>
   );
---
+++ src/components/TodoForm/TodoForm.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button,Form } from "react-bootstrap";
+
+function TodoForm() {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label>
+          <b>Add Todo</b>
+        </Form.Label>
+        <Form.Control
+          type="text"
+          className="input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Add new todo"
+        />
+      </Form.Group>
+      <Button variant="primary mt-3 mb-3" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+export default TodoForm;
