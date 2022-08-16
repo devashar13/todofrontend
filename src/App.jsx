38f8b8cf50918d1becc4ea9a08dde96ff75f841f
Dev Ashar <dev.ashar2019@vitstudent.ac.in>
2022-08-16 11:22:08 +0530

    feat: todo heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React, {useState, useEffect} from 'react';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Button, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoInput, setTodoInput] = useState("");
   return (
-    <div className="App">
-        <Todo/>
+    <div className="app">
+      <div className="container">
+        <h1 className="text-center mb-4">Todo List</h1>
+      </div>
     </div>
   );
 }
